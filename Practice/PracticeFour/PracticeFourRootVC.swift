a1bd1ea4e9ada51ee4bacef89e9e6026ea81cf66
doyunnn <73818291+doyunnn@users.noreply.github.com>
2022-01-12 15:22:30 +0900

    [FEAT] operator 추가

--- Practice/PracticeFour/PracticeFourRootVC.swift
+++ Practice/PracticeFour/PracticeFourRootVC.swift
@@ -19,17 +19,25 @@ class PracticeFourRootVC: UIViewController {
     
     func createObservable() -> Observable<String>{
         // 비동기로 생기는 데이터를 Observable로 감싸서 리턴
-        return Observable.create(){ data in
+//        return Observable.just("Hello RxSwfit") // just hello swift
+        return Observable.from(["Hello","Swift"]) // from  - hello \n swift
+        
+        
+        // 풀어서 쓴 코드 
+        /*return Observable.create(){ data in
             data.onNext("hello")
             data.onNext("rxswift")
             data.onCompleted()
             return Disposables.create()
-        }
+        }*/
     }
     
     func runObservable(){
         createObservable()
-            .subscribe{ event in
+            .subscribe(onNext: {print($0)})
+        
+        // 풀어서 쓴 코드
+        /* .subscribe{ event in
                 switch event {
                 case let .next(data):
                     print(data)
@@ -38,7 +46,7 @@ class PracticeFourRootVC: UIViewController {
                 case .error :
                     break
                 }
-            }
+            }*/
     }
     
 
