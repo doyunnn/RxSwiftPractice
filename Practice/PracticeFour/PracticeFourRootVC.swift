036d7279c04f8c8a92c227241a41cea1e25124a3
doyunnn <73818291+doyunnn@users.noreply.github.com>
2022-01-12 14:36:35 +0900

    [FEAT] RxSwift 기초

--- Practice/PracticeFour/PracticeFourRootVC.swift
+++ Practice/PracticeFour/PracticeFourRootVC.swift
@@ -6,13 +6,39 @@
 //
 
 import UIKit
+import RxSwift
 
 class PracticeFourRootVC: UIViewController {
 
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
-      
+        runObservable()
+    }
+    
+    func createObservable() -> Observable<String>{
+        // 비동기로 생기는 데이터를 Observable로 감싸서 리턴
+        return Observable.create(){ data in
+            data.onNext("hello")
+            data.onNext("rxswift")
+            data.onCompleted()
+            return Disposables.create()
+        }
+    }
+    
+    func runObservable(){
+        createObservable()
+            .subscribe{ event in
+                switch event {
+                case let .next(data):
+                    print(data)
+                case .completed :
+                    break
+                case .error :
+                    break
+                }
+            }
     }
     
 
